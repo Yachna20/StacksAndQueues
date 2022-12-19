ff2450ef63b6f89775ec2248cdcd3904b7e2f11d
Yachna20 <thakreyachna20@gmail.com>
2022-12-19 14:30:10 +0530

    [Yachna]-Ability to create a Stack and Push

---
+++ StacksandQueues/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Xml.Linq;
+
+namespace StacksandQueues
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Stack_Ex stack_ = new Stack_Ex();
+            stack_.Push(70);
+            stack_.Push(30);
+            stack_.Push(56);
+            stack_.Display();
+        }
+    }
+}
